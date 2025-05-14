0d84e923b94c34485d7a171cab3a8d2aa883c5b9
katsup <brett_7y7@yahoo.ca>
2025-05-14 15:31:49 +0900

    Update styles

--- client/src/components/ActionButtons.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-// All functionality has been merged into Toolbar.tsx
-// This component can be safely removed or kept empty.
-const ActionButtons: React.FC = () => {
-  return null;
-};
-
-export default ActionButtons;
